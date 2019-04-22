f213dd2be69d8957fa56961e324e5e04dc5cd0ec
GabrielMoraisReis <49825001+GabrielMoraisReis@users.noreply.github.com>
2019-04-22 12:15:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "patricia.h"
 #include <sys/time.h>
 #include <string.h>
+#include "lista2.h"
 
 // nao precisa, ja esta definido em patricia.h
 //#define D 8
@@ -11,23 +12,27 @@
 int main(int argc, char *argv[]) {
     TipoArvore a;
     TipoChave *k;
+    lista2 total_pal;
     int docatual=1;
     int b = 0,c=0;
     Inicializa(&a);
+    inicizalizaLista2(&total_pal);
+    criacelula2(&total_pal, docatual);
     while(b != 5) {
         insertWord(&k);
-        //printf("%s", k);
-        a = Insere(k, &a,docatual);
-        //printf("\nCHECAGEM: %c", (*a).NO.NInterno.Letra); // tem algum erro porque a primeira palavra nao deveria ter uma letra...
+        printf("%s", k);
+        a = Insere(k, &a,docatual, &total_pal);
+        printf("\nCHECAGEM: %c", (*a).NO.NInterno.Letra); // tem algum erro porque a primeira palavra nao deveria ter uma letra...
         b++;
         //printf("\n%d\n",strlen(k));
     }
     docatual=2;
+    criacelula2(&total_pal, docatual);
     while(c != 5) {
         insertWord(&k);
-        //printf("%s", k);
-        a = Insere(k, &a,docatual);
-        //printf("\nCHECAGEM: %c", (*a).NO.NInterno.Letra); // tem algum erro porque a primeira palavra nao deveria ter uma letra...
+        printf("%s", k);
+        a = Insere(k, &a,docatual, &total_pal);
+        printf("\nCHECAGEM: %c", (*a).NO.NInterno.Letra); // tem algum erro porque a primeira palavra nao deveria ter uma letra...
         c++;
         //printf("\n%d\n",strlen(k));
     }
@@ -37,4 +42,5 @@ int main(int argc, char *argv[]) {
     Pesquisa(&a, "cacada");
     Pesquisa(&a, "cacador");
     Pesquisa(&a, "true");
+    imprimeLista2(&total_pal);
 }
