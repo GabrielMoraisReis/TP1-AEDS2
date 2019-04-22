a1180396e956fa694759c72a92aff8e56996df68
HedShot <49842564+HedShot@users.noreply.github.com>
2019-04-22 18:22:14 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <sys/time.h>
 #include <string.h>
 
+
 // nao precisa, ja esta definido em patricia.h
 //#define D 8
 
@@ -11,24 +12,38 @@
 int main(int argc, char *argv[]) {
     TipoArvore a;
     TipoChave *k;
+    lista2 total_pal;
+    lista3 pesos;
     int docatual=1;
     int b = 0,c=0;
     Inicializa(&a);
-    while(b != 5) {
+    inicizalizaLista2(&total_pal);
+    criacelula2(&total_pal, docatual);
+    inicizalizaLista3(&pesos);
+    while(b != 13) {
         insertWord(&k);
         printf("%s", k);
-        a = Insere(k, &a,docatual);
+        a = Insere(k, &a,docatual, &total_pal);
         printf("\nCHECAGEM: %c", (*a).NO.NInterno.Letra); // tem algum erro porque a primeira palavra nao deveria ter uma letra...
         b++;
         //printf("\n%d\n",strlen(k));
     }
     docatual=2;
-    while(c != 5) {
+    criacelula2(&total_pal, docatual);
+    while(c != 10) {
         insertWord(&k);
         printf("%s", k);
-        a = Insere(k, &a,docatual);
+        a = Insere(k, &a,docatual, &total_pal);
         printf("\nCHECAGEM: %c", (*a).NO.NInterno.Letra); // tem algum erro porque a primeira palavra nao deveria ter uma letra...
         c++;
         //printf("\n%d\n",strlen(k));
     }
+    /*Pesquisa(&a, "banana");
+    Pesquisa(&a, "casada");
+    Pesquisa(&a, "cacada");
+    Pesquisa(&a, "cacador");
+    Pesquisa(&a, "true");*/
+   //imprimeLista2(&total_pal);
+   buscaindice(&a, docatual, &pesos, &total_pal);//buscaindice(TipoArvore *t,int docatual,lista3* pesos,lista2* total_pal
+    
 }
