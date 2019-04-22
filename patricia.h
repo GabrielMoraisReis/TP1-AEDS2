27d45c4c082fbdc2cc8e81e2b7348c28fc22a349
HedShot <49842564+HedShot@users.noreply.github.com>
2019-04-22 18:22:47 -0300

    Update patricia.h

--- patricia.h
+++ patricia.h
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include "lista.h"
+#include "lista2.h"
+#include "lista3.h"
 #define D 50   /* depende de TipoChave */
 
 typedef unsigned char TipoChave; /* a definir, dependendo da aplicacao */
@@ -34,9 +36,11 @@ typedef struct TipoPatNo {
 
 short EExterno(TipoArvore p);
 TipoArvore CriaNoInt(unsigned int i, TipoArvore *Esq,  TipoArvore *Dir,unsigned char Letra);
-TipoArvore CriaNoExt(TipoChave *k,int docatual);
-void Pesquisa(TipoChave k, TipoArvore t);
-TipoArvore InsereEntre(TipoChave *k, TipoArvore *t,unsigned int i,unsigned char Letra,int docatual);
-TipoArvore Insere(TipoChave *k, TipoArvore *t,int docatual);
+TipoArvore CriaNoExt(TipoChave *k,int docatual, lista2* total_pal);
+void Pesquisa(TipoArvore *t, TipoChave *k);
+TipoArvore InsereEntre(TipoChave *k, TipoArvore *t,unsigned int i,unsigned char Letra,int docatual, lista2* total_pal);
+TipoArvore Insere(TipoChave *k, TipoArvore *t,int docatual, lista2* total_pal);
 void Inicializa(TipoArvore *a);
 void insertWord(Tipovec* data);
+void BUSCA(TipoArvore *t, TipoChave *k,int docatual,lista3* pesos);
+void buscaindice(TipoArvore *t,int docatual,lista3* pesos,lista2* total_pal);
