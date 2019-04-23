a059f4b8d53c5194f676dd0cc7b924dc870c67f4
GabrielMoraisReis <49825001+GabrielMoraisReis@users.noreply.github.com>
2019-04-22 23:14:17 -0300

    Create Arquivo.c

---
+++ Arquivo.c
@@ -0,0 +1,27 @@
+#include "Arquivo.h"
+#include <string.h>
+#include <ctype.h>
+
+void LeArquivo(TipoArvore *t, TipoApontador *no, lista2* total_pal, int contaDoc){
+    FILE *file;
+    char  nomeArq[100];
+    char string[50];
+    int c=0;
+    printf("Digite o endereco do arquivo");
+    scanf("%s", nomeArq);
+    file=fopen(nomeArq, "r");
+    while(!feof(file)){
+        fscanf(file, "%s", string);
+        strlwr(string);
+        c = strlen(string);
+        for(int i=0;i<c;i++){
+            if(ispunct(string[i]))
+                string[i]=NULL;
+        }
+        Insere(string, t, contaDoc, total_pal);
+        InsereNo(string, no);
+    }
+
+    fclose(file);
+}
+
