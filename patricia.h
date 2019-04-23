25d1058b6636fe61440eb4eb3f0354c500437139
sw3luke <luke3skywalker@gmail.com>
2019-04-22 21:44:05 -0300

    Create patricia.h

---
+++ patricia.h
@@ -0,0 +1,42 @@
+#include<stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+#define D 50   /* depende de TipoChave */
+
+typedef unsigned char TipoChave; /* a definir, dependendo da aplicacao */
+typedef unsigned char TipoIndexAmp;
+
+// desabilitei pois nao esta usando o Bit mais
+//typedef unsigned char TipoDib;
+
+typedef TipoChave* Tipovec;
+typedef enum {
+    Interno, Externo
+} TipoNo;
+typedef struct TipoPatNo* TipoArvore;
+typedef struct TipoPatNo {
+    TipoNo nt;
+    union {
+        struct {
+            TipoIndexAmp Pos,Letra;
+            TipoArvore Esq, Dir;
+        } NInterno ;
+        TipoChave Chave[50];
+    } NO;
+} TipoPatNo;
+
+// desabilitei pois nao esta usando o Bit mais
+//TipoDib Bit(TipoIndexAmp i, TipoChave k);
+
+short EExterno(TipoArvore p);
+TipoArvore CriaNoInt(unsigned int i, TipoArvore *Esq,  TipoArvore *Dir,unsigned char Letra);
+TipoArvore CriaNoExt(TipoChave *k);
+TipoArvore InsereEntre(TipoChave *k, TipoArvore *t,unsigned int i,unsigned char Letra);
+TipoArvore Insere(TipoChave *k, TipoArvore *t);
+void Inicializa(TipoArvore *a);
+void insertWord(Tipovec* data);
+void Ordem(TipoArvore no);
+void Pesquisa(TipoArvore *t, TipoChave *k);
+unsigned char InsereEntreLetra(TipoArvore *t,unsigned int i, unsigned char letra);
+TipoArvore* checagem(TipoArvore *p);
+void ImprimePat(TipoArvore t);
